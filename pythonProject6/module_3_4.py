0f2460abdf839f74eb30eed0b16e27493fa6138f
Llina-moon <Llina.moon.luna@gmail.com>
2024-09-26 11:15:10 +0300

    25.09 26.09

--- pythonProject6/module_3_4.py
+++ pythonProject6/module_3_4.py
@@ -1,12 +1,12 @@
 def single_root_words(root_word , *other_words):
     same_words = []
     for i in other_words:
-        if root_word.lower() in i.lower():
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
             same_words.append(i)
 
     return same_words
 
 a = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies' )
 print(a)
-b = single_root_words('Able', 'Disablement','Able', 'Mable', 'Disable', 'Bagel')
+b = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(b)

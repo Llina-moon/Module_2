568d1e07a89bff6690128efcb087d31369475e80
Llina-moon <Llina.moon.luna@gmail.com>
2024-11-29 11:11:06 +0300

    29.11

---
+++ module9/module_9_1.py
@@ -0,0 +1,30 @@
+def apply_all_func(int_list, *functions):
+    result ={}
+    for func in functions:
+        name = func.__name__
+        result[name] = func(int_list)
+    return result
+
+
+def min(int_list):
+    result = int_list[0]
+    for i in int_list:
+        if i < result:
+            result = i
+    return result
+
+def max(int_list):
+    result = int_list[0]
+    for i in int_list:
+        if i > result:
+            result = i
+    return result
+
+def sum_(int_list):
+    return sum(int_list)
+
+def sorted_(int_list):
+    return sorted(int_list)
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
